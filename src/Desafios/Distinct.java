039a0e500745ae9fb2bc573111b65c749f07bafa
Bowi3 <leonnardofender@gmail.com>
2024-06-06 10:36:14 -0300

    Desafio distinct

---
+++ src/Desafios/Distinct.java
@@ -0,0 +1,14 @@
+package Desafios;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Distinct {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        List<Integer> result = numeros.stream().distinct().toList();
+
+        System.out.println(result);
+    }
+}
