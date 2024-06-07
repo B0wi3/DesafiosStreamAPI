6a0ea80c62c11140bfb16a38b9a973caa2317503
Bowi3 <leonnardofender@gmail.com>
2024-06-07 10:03:10 -0300

    Desafio filtrar numeros por intervalo

---
+++ src/Desafios/FiltrarIntervalo.java
@@ -0,0 +1,13 @@
+package Desafios;
+
+import java.util.*;
+
+public class FiltrarIntervalo {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        List<Integer> result = numeros.stream().filter(n -> (n > 5) && (n < 10)).toList();
+
+        System.out.println(result);
+    }
+}
