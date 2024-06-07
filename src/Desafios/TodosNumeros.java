1d4cd92f9181c720684e9ea83536e80227b8c613
Bowi3 <leonnardofender@gmail.com>
2024-06-07 09:45:30 -0300

    Corrigir desafio mostrar todos numeros

--- src/Desafios/TodosNumeros.java
+++ src/Desafios/TodosNumeros.java
@@ -3,11 +3,11 @@ package Desafios;
 import java.util.Arrays;
 import java.util.List;
 
-public class Distinct {
+public class TodosNumeros {
     public static void main(String[] args) {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
 
-        List<Integer> result = numeros.stream().distinct().toList();
+        List<Integer> result = numeros.stream().sorted().toList();
 
         System.out.println(result);
     }
---
+++ src/Desafios/VerificaDistintos.java
@@ -0,0 +1,9 @@
+package Desafios;
+
+import java.util.*;
+
+public class VerificaDistintos {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+    }
+}
