905ad7c6b77aa4369c08f648da4cf59c172de703
Bowi3 <leonnardofender@gmail.com>
2024-06-07 09:27:22 -0300

    Desafio encontrar segundo maior item

---
+++ src/Desafios/SegundoMaior.java
@@ -0,0 +1,17 @@
+package Desafios;
+
+import java.util.*;
+
+public class SegundoMaior {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        Optional<Integer> segundoMaior = numeros.stream()
+                .distinct()
+                .sorted((a, b) -> b - a)
+                .skip(1)
+                .findFirst();
+
+        System.out.println(segundoMaior);
+    }
+}
