d2aabd71a50b85294a1459c4ba757a144d973c26
Bowi3 <leonnardofender@gmail.com>
2024-06-07 09:22:27 -0300

    Desafio verificar se há números maiores que 10

---
+++ src/Desafios/VerificaMaiorQue10.java
@@ -0,0 +1,12 @@
+package Desafios;
+
+import java.util.*;
+
+public class VerificaMaiorQue10 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        boolean result = numeros.stream().anyMatch(n -> n > 10);
+        System.out.println(result);
+    }
+}
