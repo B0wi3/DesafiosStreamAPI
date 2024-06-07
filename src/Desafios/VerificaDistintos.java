f5063550803c52fc9420962f610f34037c2d466d
Bowi3 <leonnardofender@gmail.com>
2024-06-07 09:50:17 -0300

    Desafio distintos

--- src/Desafios/VerificaDistintos.java
+++ src/Desafios/VerificaDistintos.java
@@ -5,5 +5,15 @@ import java.util.*;
 public class VerificaDistintos {
     public static void main(String[] args) {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        long result = numeros.stream().distinct().count();
+
+        System.out.println(result + " distintos.");
+
+        if (result < numeros.size()) {
+            System.out.println("Nem todos são distintos.");
+        } else {
+            System.out.println("Todos são distintos.");
+        }
     }
 }
